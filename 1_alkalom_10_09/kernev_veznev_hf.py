05af9328c93f4d530d6fd52834297ddbd323e1e0
vincedulicz <168646809+vincedulicz@users.noreply.github.com>
2024-10-10 10:52:01 +0200

    Update kernev_veznev_hf.py

--- 1_alkalom_10_09/kernev_veznev_hf.py
+++ 1_alkalom_10_09/kernev_veznev_hf.py
@@ -11,3 +11,18 @@ def second_task_unsolved():
   return False
 
 # 2 task end
+
+# 3 task begin
+
+var = 0
+if var == 0:
+  return "ok"
+else:
+  return "not ok"
+
+var += 1
+name = "Stringes Námmme"
+
+print(f'hi {name}! Are u {var}?')
+
+# 3 task end
